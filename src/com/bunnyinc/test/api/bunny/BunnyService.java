098684e0631e90c3f00f9972bee91d1d19ad4d1e
Juan Rodrigo Torres <juanro.toes@gmail.com>
2015-03-28 10:46:52 -0500

    First Structure
    Principal structure and files.

---
+++ src/com/bunnyinc/test/api/bunny/BunnyService.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.bunnyinc.test.api.bunny;
+
+/**
+ * @author Rodrigo
+ *
+ */
+public interface BunnyService {
+
+}
---
+++ src/com/bunnyinc/test/api/external/ExternalService.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.bunnyinc.test.api.external;
+
+/**
+ * @author Rodrigo
+ *
+ */
+public interface ExternalService {
+
+}
